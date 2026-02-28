9bf1502f4f33ec997f22c697b9fdf6271c048357
hellowaste344 <xashe107@gmail.com>
2026-02-28 17:07:42 +0300

    new commit

---
+++ pathlib_test.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+p = Path("..")
+print(p.cwd())
+
+for subdir in p.iterdir():
+    if subdir.is_dir():
+        print(subdir)
+
+py_files = p.rglob("*.py")
+
+# for f in py_files:
+# print(f)
+
+sf = p / "users.db"
+print(sf)
+print("Is absolute? ", sf.is_absolute())
+print("Is exist? ", sf.exists())
+print("File name: ", sf.name)
+print("Extensions: ", sf.suffix)
+print("Parent directory: ", sf.parent)
+
+
+# reading from a file
+with (p / "banana.txt").open("w") as f:
+    f.write("Welcome to My darkness")
+
+with (p / "banana.txt").open("r") as f:
+    print(f.read())
